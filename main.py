cf60bf1c74195cf424ac569a4e0e36df1026ef15
Obianuju Anosike <101943732+Obianuju1998@users.noreply.github.com>
2024-03-27 20:10:16 +0000

    Uploaded an updated version of QuotesApp
    The changes made will be able to tell the user when they enter a keyword not in the API. This way, they are notified through an error message and encouraged to try again.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
 
-api = "PkGY0P0q4Xt4OceCIAhNEg==y5LhNNiQae3UjxrP"
-category = 'happiness'
-api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
-response = requests.get(api_url, headers={'X-Api-Key': api})
+CATEGORY = input("What do you need a quote on? Enter one word. E.g, happiness: ").lower()
+API_KEY = "PkGY0P0q4Xt4OceCIAhNEg==y5LhNNiQae3UjxrP"
+API_URL = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(CATEGORY)
+
+
+response = requests.get(API_URL, headers={'X-Api-Key': API_KEY})
 if response.status_code == requests.codes.ok:
     quote = response.text
-    quote_info = quote.split(":")
-    main_quote_info = quote_info[1].split(".")[0]
-    apostrophe = '"'
-    author_quote = quote_info[-2].split(",")[0][2:-1]
-    print(f"{main_quote_info}.{apostrophe} - By {author_quote}.")
+    if len(quote) == 0:
+        quote_info = quote.split(":")
+        main_quote_info = quote_info[1].split(".")[0]
+        apostrophe = '"'
+        author_quote = quote_info[-2].split(",")[0][2:-1]
+        print(f"{main_quote_info}.{apostrophe} - By {author_quote}.")
+    else:
+        print("This keyword does not exist in the API!!! Kindly try again.")
 else:
     print("Error:", response.status_code, response.text)
